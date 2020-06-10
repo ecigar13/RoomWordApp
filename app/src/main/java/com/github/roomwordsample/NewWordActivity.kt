be32881c66bee94c70aedec6cffed3941e45ac6a
Keith, Nguyen <keith.nguyen@sabre.com>
2020-06-09 23:45:44 -0500

    First commit.

---
+++ app/src/main/java/com/github/roomwordsample/NewWordActivity.kt
@@ -0,0 +1,44 @@
+package com.github.roomwordsample
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+
+class NewWordActivity : AppCompatActivity() {
+
+    private lateinit var editWordView: EditText
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+        editWordView = findViewById(R.id.edit_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editWordView.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+}
